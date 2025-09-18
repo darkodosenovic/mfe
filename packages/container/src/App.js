ad60eff4c70804ef02f33b2c7b7bded6e9588fcc
darko.dosenovic@ama.ab.ca <darko.dosenovic@ama.ab.ca>
2025-09-18 11:12:23 -0600

    Adding Lazy Load process.

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Switch} from "react-router-dom";
-import MarketingApp from "./components/MarketingApp";
-import AuthApp from "./components/AuthApp";
+
+import Progress from "./components/Progress";
 import Header from "./components/Header";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
+
 import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 
 const generateClassName = createGenerateClassName({
@@ -15,10 +19,12 @@ export default () => {
           <StylesProvider generateClassName={generateClassName}>
             <div>
               <Header />
-              <Switch>
-                <Route path="/auth" component={AuthApp} />
-                <Route path="/" component={MarketingApp} />
-              </Switch>
+              <Suspense fallback={<Progress />}>
+                <Switch>
+                  <Route path="/auth" component={AuthLazy} />
+                  <Route path="/" component={MarketingLazy} />
+                </Switch>
+              </Suspense>
             </div>
         </StylesProvider>
       </BrowserRouter>
